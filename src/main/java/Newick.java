b5db8ef6796246c43d97a6ac2560390bb13e6593
Peter Borkuti <peter@aspire>
2017-08-29 23:10:26 +0200

    newick-not working

--- src/main/java/Newick.java
+++ src/main/java/Newick.java
@@ -1,14 +1,35 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Newick {
     private Node<String> root = null;
-    
+
     public final String newick;
-    
+
+    private String workNewick;
+
+    private String separateAtNonWordChars = "\\W";
+
+    private Matcher matcher;
+
+    private int matcherStartIndex = 0;
+
     public Newick(String newick) {
+    	newick = newick.toLowerCase();
+    	newick = newick.replaceAll("[^a-z(),]", "");
         this.newick = newick;
+        this.workNewick = newick;
+        Pattern p = Pattern.compile(separateAtNonWordChars);
+        matcher = p.matcher(newick);
+
     }
     
     public String nextNewickElement() {
-    	return "";
+    	if (!matcher.find()) {
+    		return "";
+    	}
+
+    	return newick;
     }
     
     public void parse() {
--- src/test/java/NewickTest.java
+++ src/test/java/NewickTest.java
@@ -10,8 +10,33 @@ public class NewickTest {
 	}
 
 	@Test
+	public void deleteNonAllowedChars(){
+		Newick newick = new Newick("a- .b,()[]");
+		assertEquals("ab,()", newick.newick);
+	}
+
+	@Test
+	public void toLowerCase(){
+		Newick newick = new Newick("ABC");
+		assertEquals("abc", newick.newick);
+	}
+
+	@Test
 	public void testNextForRootElement(){
 		Newick newick = new Newick("root");
 		assertEquals("root", newick.newick);
 	}
+
+	@Test
+	public void testNextNewickElement(){
+		Newick newick = new Newick("root");
+		assertEquals("root", newick.nextNewickElement());
+		newick = new Newick("(");
+		assertEquals("(", newick.nextNewickElement());
+		newick = new Newick("root(");
+		assertEquals("root", newick.nextNewickElement());
+		assertEquals("(", newick.nextNewickElement());
+
+	}
+	
 }
