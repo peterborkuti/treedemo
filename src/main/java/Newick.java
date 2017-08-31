95fa621a11214c2002d73906650ac021b74d458e
Peter Borkuti <peter@aspire>
2017-08-31 19:56:26 +0200

    before starting parser

--- src/main/java/Newick.java
+++ src/main/java/Newick.java
@@ -2,30 +2,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Newick {
-    private Node<String> root = null;
+	private Node<String> root = null;
 
-    public final String newick;
+	public final String newick;
 
-    private String separateAtWholeWordOrOneCharNonWord = "\\W{1}|\\w+";
+	private String separateAtWholeWordOrOneCharNonWord = "\\W{1}|\\w+";
 
-    private Matcher matcher;
-    private int matcherIndex = 0;
-    private boolean shouldExecuteFind = true;
+	private Matcher matcher;
+	private int matcherIndex = 0;
+	private boolean shouldExecuteFind = true;
 
-    public Newick(String newick) {
-    	newick = newick.toLowerCase();
-    	newick = newick.replaceAll("[^a-z0-9(),]", "");
-        this.newick = newick;
-        Pattern p = Pattern.compile(separateAtWholeWordOrOneCharNonWord);
-        matcher = p.matcher(newick);
-    }
+	public Newick(String newick) {
+		newick = newick.toLowerCase();
+		newick = newick.replaceAll("[^a-z0-9(),]", "");
+		this.newick = newick;
+		Pattern p = Pattern.compile(separateAtWholeWordOrOneCharNonWord);
+		matcher = p.matcher(newick);
+	}
 
-    private String nextNewickElement() {
-   		return (matcher.find()) ? newick.substring(matcher.start(), matcher.end()) : "";
-    }
+	private String nextNewickElement() {
+		return (matcher.find()) ? newick.substring(matcher.start(), matcher.end()) : "";
+	}
 
-    public void parse() {
-        
-    }
-    
 }
--- src/test/java/NewickTest.java
+++ src/test/java/NewickTest.java
@@ -1,14 +1,33 @@
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
 
 public class NewickTest {
-	private Newick newick;
-	Method method = newick.getClass().getDeclaredMethod("nextNewickElement");
-	method.setAccessible(true);
-	Object r = method.invoke(object);
+	private String invokeNextNewickElement(Newick newick) {
+		Method method = null;
+		try {
+			method = newick.getClass().getDeclaredMethod("nextNewickElement");
+		} catch (NoSuchMethodException | SecurityException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		method.setAccessible(true);
+
+		Object newickElement = null;
+		try {
+			newickElement = method.invoke(newick);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return newickElement == null ? "" : (String)newickElement;
+	}
+
 	@Test
 	public void instantiationTest(){
 		Newick newick = new Newick("x");
@@ -36,25 +55,25 @@ public class NewickTest {
 	@Test
 	public void testNextNewickElementForOneElement(){
 		Newick newick = new Newick("root");
-		assertEquals("only root element", "root", newick.nextNewickElement());
+		assertEquals("only root element", "root", invokeNextNewickElement(newick));
 	}
 
 	@Test
 	public void testTwoClosingBrackets(){
 		Newick newick = new Newick("))");
-		assertEquals("first bracket", ")", newick.nextNewickElement());
-		assertEquals("second bracket", ")", newick.nextNewickElement());
-		assertEquals("reading after end", "", newick.nextNewickElement());
+		assertEquals("first bracket", ")", invokeNextNewickElement(newick));
+		assertEquals("second bracket", ")", invokeNextNewickElement(newick));
+		assertEquals("reading after end", "", invokeNextNewickElement(newick));
 	}
 
 	@Test
 	public void testNextNewickElementForTree(){
 		Newick newick = new Newick("root(child)");
-		assertEquals("root", "root", newick.nextNewickElement());
-		assertEquals("(", "(", newick.nextNewickElement());
-		assertEquals("child", "child", newick.nextNewickElement());
-		assertEquals(")", ")", newick.nextNewickElement());
-		assertEquals("reading after end", "", newick.nextNewickElement());
+		assertEquals("root", "root", invokeNextNewickElement(newick));
+		assertEquals("(", "(", invokeNextNewickElement(newick));
+		assertEquals("child", "child", invokeNextNewickElement(newick));
+		assertEquals(")", ")", invokeNextNewickElement(newick));
+		assertEquals("reading after end", "", invokeNextNewickElement(newick));
 	}
 
 	@Test
@@ -63,7 +82,7 @@ public class NewickTest {
 		String[] parts = {"root", "(", "child1",",", "child2","(", "subchild", ")", ")", ""};
 
 		for (int i = 0; i < parts.length; i++) {
-			assertEquals(i + ":" + parts[i], parts[i], newick.nextNewickElement());
+			assertEquals(i + ":" + parts[i], parts[i], invokeNextNewickElement(newick));
 		}
 	}
 }
